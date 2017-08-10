bcf79e6889f10e299ca213d9b9f4034014f5b15a
Wiest <dwiest@C02S6STFFVH5.attlocal.net>
2017-08-09 20:14:09 -0500

    Fixed bug that was causing an infinite connection attempt loop

--- libs/spotify.js
+++ libs/spotify.js
@@ -2,7 +2,7 @@ const https = require('https');
 const SpotifyControl = require('spotify-control');
 
 module.exports = class Spotify {
-	static generateToken() {
+	static retrieveToken() {
 		return new Promise((resolve, reject) => {
 			const options = {
 				host: 'open.spotify.com',
@@ -30,45 +30,53 @@ module.exports = class Spotify {
 	constructor() {
 		this.spotify = null;
 		this.connected = false;
+		this.status = {
+			online: false,
+		};
 
 		this.connect();
 	}
 
 	connect() {
-		this.setConnectedStatus(false);
-		Spotify.generateToken().then((token) => {
+		Spotify.retrieveToken().then((token) => {
 			this.spotify = new SpotifyControl({
 				token,
 			});
 			this.spotify.connect().then(() => {
 				this.updatePlaybackStatus();
 			})
+				// Failed to connect to Spotify
 				.catch(() => setTimeout(this.connect.bind(this), 1000));
 		})
+			// Failed to retrieve Spotify token
 			.catch(() => setTimeout(this.connect.bind(this), 1000));
 	}
 
 	updatePlaybackStatus() {
 		this.spotify.status().then((result) => {
-			if (result && result.online) {
-				this.setConnectedStatus(true);
-				setTimeout(this.updatePlaybackStatus.bind(this), 100);
-			} else this.setConnectedStatus(false);
+			this.setIsConnected(true);
+			if (result.online) this.setIsOnline(true);
+			else this.setIsOnline(false);
+
+			setTimeout(this.updatePlaybackStatus.bind(this), 100);
 		})
 			.catch(() => {
-				this.setConnectedStatus(false);
-				this.connect();
+				this.setIsConnected(false);
+				setTimeout(this.connect.bind(this), 1000);
 			});
 	}
 
-	setConnectedStatus(status) {
-		if (status !== this.connected) {
+	setIsConnected(status) {
+		if (this.connected !== status) {
 			this.connected = status;
 			if (this.connected) console.log('Established connection to Spotify client');
-			else {
-				console.log('Lost connection to Spotify client');
-				setTimeout(this.connect.bind(this), 1000);
-			}
+			else console.log('Lost connection to Spotify client');
+		}
+	}
+
+	setIsOnline(online) {
+		if (this.status.online !== online) {
+			this.status.online = online;
 		}
 	}
 };
