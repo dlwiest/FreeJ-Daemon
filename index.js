83cd97086397494c773624f67ba8e7a6446224d0
Wiest <dwiest@C02S6STFFVH5.attlocal.net>
2017-08-12 19:09:04 -0500

    Fixed EventEmitter memory leak

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ const io = require('socket.io')(server);
 const spotify = new Spotify();
 
 spotify.on('playbackStatusChanged', (change) => {
-	//console.log(change);
+	io.emit('playbackStatusChanged', change);
 });
 
 io.on('connection', (client) => {
 	client.emit('playbackStatus', spotify.status);
-
-	spotify.on('playbackStatusChanged', (change) => {
-		client.emit('playbackStatusChanged', change);
-	});
-
-	client.on('disconnect', () => {
-		// Disconnect code
-	});
 });
 
 
